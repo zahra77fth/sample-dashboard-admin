6a34c3e2b2e02e7a4ab00df6b95f64b3af44a561
zahra77fth <zhr77fth@gmail.com>
2021-05-31 23:30:18 +0430

    added sider to user-sider

--- layouts/user-sider.vue
+++ layouts/user-sider.vue
@@ -15,7 +15,7 @@
                     ></v-text-field>
                     </v-btn>
                 <div class="navbar_right-icons">
-                
+                <v-icon>mdi-cog-outline</v-icon>
                 </div>
             </v-form>
             <div class="navbar_user-info">
@@ -25,12 +25,40 @@
         <div class="page-content">
             <div class="sider" >
                 <div class="sider_header"></div>
-                <NuxtLink to="/forget-password" class="text-center"> My Day </NuxtLink>
-                <NuxtLink to="/forget-password" class="text-center"> Important </NuxtLink>
-                <NuxtLink to="/forget-password" class="text-center"> Planned </NuxtLink>
-                <NuxtLink to="/forget-password" class="text-center"> Assigned to you</NuxtLink>
-                <NuxtLink to="/forget-password" class="text-center"> Tasks</NuxtLink>
-
+      <v-navigation-drawer
+        permanent
+      >
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon>mdi-weather-sunny</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>My Day</v-list-item-title>
+          </v-list-item>
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon>mdi-star-outline</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Important</v-list-item-title>
+          </v-list-item>
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon>mdi-calendar-month-outline</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Planned</v-list-item-title>
+          </v-list-item>
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon>mdi-account-outline</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Asigned to you</v-list-item-title>
+          </v-list-item>
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon>mdi-home-variant-outline</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Tasks</v-list-item-title>
+          </v-list-item>
+      </v-navigation-drawer>
             </div>
             <div class="ground">
                 <Nuxt />
@@ -73,8 +101,23 @@ background-color: #f4f4f4;
 .sider {
     width: 50px;
 }}
-.ground{
+.v-navigation-drawer{
+    height: 100%;
+    width: 290px !important;
+    background-color: #f4f4f4;
+}
+@media (max-width: 1010px){
+.v-navigation-drawer {
+    width: 50px;
+}}   
 
+.sider_menu{
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: left;
+    align-items: flex-start;
+    color: #707d88 !important;
 }
 .todo{
     width: 480px;
